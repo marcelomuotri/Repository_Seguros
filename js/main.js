97b9999a412bfedaf95e558ae676f721bb3a063a
Marcelo Muotri <muotrimarcelo@gmail.com>
2021-01-25 16:23:06 +0000

    actualizaciones

--- js/main.js
+++ js/main.js
@@ -1,39 +1,23 @@
-function agregarAuto(){
+    function Autos(marca, modelo , anio){/* CREO EL CONSTRUCTOR */
+        this.marca = marca;
+        this.modelo = modelo;
+        this.anio = anio;
+    
+    }
+    /* CREO LAS VARIABLES PARA AGREGAR */    
+    //OBJETOS TIPO AUTO//
+    var aniosAutos= ["1990" , "1991" , "1992" , "1993" , "1994"];
+    primerAuto = new Autos ("Peugeot", ["206", "207", "208", "306" , "307" , "308"], aniosAutos);
 
-function Autos(marca, modelo , anio){/* CREO EL CONSTRUCTOR */
-    this.marca = marca;
-    this.modelo = modelo;
-    this.anio = anio;
+    segundoAuto = new Autos ("Chevrolet", ["corsa" , "agile" , "onix" , "camaro"], aniosAutos);
 
-}
-/* CREO LAS VARIABLES PARA AGREGAR */
-var capturadorMarca = (prompt("pone tu marca"));
-var capturadorModelo = (prompt("pone tu modelo"));
-var capturadorAnio = (prompt("pone tu anio"));
-
-PrimerAuto = new Autos (capturadorMarca, capturadorModelo, capturadorAnio);
-console.log(PrimerAuto);
-/* AGREGO LAS VARIABLES AL OBJETO */
-agregar();
-}
-
-/* CREO LA BASE DE DATOS */
-
-BaseDatosAuto = [];
-
-/* AGREGO LAS COSAS DESDE EL OBJETO A LA BASE DE DATOS */
-
-function agregar(){
-    BaseDatosAuto.push(PrimerAuto);
-    console.log(BaseDatosAuto)
-}
-
-agregarAuto();
-
-/* PREGUNTO SI QUIERE AGREGAR OTRO AUTO */
-
-otro = prompt("quiere agregar otro auto ? ")  ;
-while ( otro == "si"){
-    agregarAuto();
-    otro = prompt("quiere agregar otro auto");
-}
+    
+    BaseDatosAuto = [primerAuto, segundoAuto];
+    /* console.log(BaseDatosAuto) */
+    console.log(BaseDatosAuto[0].anio);   
+        
+   
+    
+   
+    
+ 
