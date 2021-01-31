95a1e27e6a5f96366b47d4dfe5263176ca730cb8
Marcelo Muotri <muotrimarcelo@gmail.com>
2021-01-31 23:16:31 +0000

    Trabajo Numero 8

--- js/main.js
+++ js/main.js
@@ -8,15 +8,36 @@
     }
         
     //OBJETOS TIPO AUTO//
-    var aniosAutos= ["1990" , "1991" , "1992" , "1993" , "1994"];
-    primerAuto = new Autos ("Peugeot", ["206", "207", "208", "306" , "307" , "308"], aniosAutos);
+     aniosAutos= ["1990" , "1991" , "1992" , "1993" , "1994", "1995","1996", "1997"];
+    primerAuto = new Autos ("Peugeot", ["505" ,"206", "207", "208", "306" , "307" , "308"], aniosAutos);
 
     segundoAuto = new Autos ("Chevrolet", ["corsa" , "agile" , "onix" , "camaro"], aniosAutos);
-
     
     BaseDatosAuto = [primerAuto, segundoAuto];
-    
-    console.log(BaseDatosAuto[0].anio);   
+       
+   var select = document.getElementById("exampleFormControlSelect3"); //Seleccionamos el select
+      
+      for(var i=0; i < BaseDatosAuto[0].anio.length; i++){ 
+          var option = document.createElement("option"); //Creamos la opcion
+          option.innerHTML = BaseDatosAuto[0].anio[i]; //Metemos el texto en la opción
+          select.appendChild(option); //Metemos la opción en el select
+      }
+  
+     
+   var select = document.getElementById("exampleFormControlSelect1"); //Seleccionamos el select
    
-    
+      for(var i=0; i < BaseDatosAuto.length; i++){ 
+         var option = document.createElement("option"); //Creamos la opcion
+         option.innerHTML = BaseDatosAuto[i].marca; //Metemos el texto en la opción
+         select.appendChild(option); //Metemos la opción en el select
+      }
+
+   var select = document.getElementById("exampleFormControlSelect2"); //Seleccionamos el select
+      
+      for(var i=0; i < BaseDatosAuto[0].modelo.length; i++){ 
+          var option = document.createElement("option"); //Creamos la opcion
+          option.innerHTML = BaseDatosAuto[0].modelo[i]; //Metemos el texto en la opción
+          select.appendChild(option); //Metemos la opción en el select
+      }
+
     
