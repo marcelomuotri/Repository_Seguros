99b3eaf320f9008d10a71efc0c056e1b83a73e18
Marcelo Muotri <muotrimarcelo@gmail.com>
2021-02-03 00:20:06 +0000

    eventos

--- js/main.js
+++ js/main.js
@@ -1,28 +1,76 @@
-  
+function rango(primerAnio,segundoAnio){
+   var primerAnio = primerAnio;
+   var segundoAnio = segundoAnio;
+   aniosAutos=[]
+   for( var i = primerAnio ; i <= segundoAnio ; i++){ 
+      aniosAutos.push(i);
+   }
+   return aniosAutos;
+}  
 
-  function Autos(marca, modelo , anio){//CREO EL CONSTRUCTOR
+  function Autos(marca, modelo ){//CREO EL CONSTRUCTOR
         this.marca = marca;
         this.modelo = modelo;
-        this.anio = anio;
     
     }
-        
-    //OBJETOS TIPO AUTO//
-     aniosAutos= ["1990" , "1991" , "1992" , "1993" , "1994", "1995","1996", "1997"];
-    primerAuto = new Autos ("Peugeot", ["505" ,"206", "207", "208", "306" , "307" , "308"], aniosAutos);
+    function Cmodelo (modelo, valor, anios){
+       this.modelo = modelo;
+       this.valor = valor;
+       this.anios = anios;
 
-    segundoAuto = new Autos ("Chevrolet", ["corsa" , "agile" , "onix" , "camaro"], aniosAutos);
+    }
+    //OBJETOS TIPO MODELO\\
+    //OBJETOS TIPO AUTO///// CARGO MARCA, MODELO(OBJETO TIPO CMODELO(MODELO, VALOR) ,ANIO ), 
+     primerAuto = new Autos ("PEUGEOT", [
+      new Cmodelo ("504" , 5000, rango(1990,1995)), 
+      new Cmodelo ("206" , 6000, rango(1998,2012)), 
+      new Cmodelo ("207" , 7000, rango(2007,2021)),
+      new Cmodelo ("208" , 8000, rango(2015,2021)),
+      new Cmodelo ("306" , 9000, rango(1990,2005)),
+      ]); 
+
+    segundoAuto = new Autos ("CHEVROLET", [
+      new Cmodelo ("CORSA" , 5000, rango(1990,1995)), 
+      new Cmodelo ("AGILE" , 6000, rango(1998,2012)), 
+      new Cmodelo ("ONIX" , 7000, rango(2007,2021)),
+      new Cmodelo ("CAMARO" , 8000, rango(2015,2021)),
+      new Cmodelo ("ASTRA" , 9000, rango(1990,2005)),  
+      ]);
+
+    tercerAuto = new Autos ("FORD", [
+      new Cmodelo ("FIESTA" , 5000, rango(1990,1995)), 
+      new Cmodelo ("FOCUS" , 6000, rango(1998,2012)), 
+      new Cmodelo ("KUGA" , 7000, rango(2007,2021)),
+      new Cmodelo ("MUSTANG" , 8000, rango(2015,2021)),
+      new Cmodelo ("NEW BEETLE" , 9000, rango(1990,2005)),  
+      ]);
+
+    cuartoAuto = new Autos ("AUDI", [
+      new Cmodelo ("A1" , 5000, rango(1990,1995)), 
+      new Cmodelo ("A2" , 6000, rango(1998,2012)), 
+      new Cmodelo ("A3" , 7000, rango(2007,2021)),
+      new Cmodelo ("A4" , 8000, rango(2015,2021)),
+      new Cmodelo ("A5" , 9000, rango(1990,2005)),
+      ]);        
+
+    quintoAuto = new Autos ("FIAT", [
+      new Cmodelo ("PUNTO" , 5000, rango(1990,1995)), 
+      new Cmodelo ("UNO" , 6000, rango(1998,2012)), 
+      new Cmodelo ("TIPO" , 7000, rango(2007,2021)),
+      new Cmodelo ("DUNA" , 8000, rango(2015,2021)),
+      new Cmodelo ("SIENA" , 9000, rango(1990,2005)),  
+      ]);                    
     
-    BaseDatosAuto = [primerAuto, segundoAuto];
-       
-   var select = document.getElementById("exampleFormControlSelect3"); //Seleccionamos el select
+    BaseDatosAuto = [primerAuto, segundoAuto, tercerAuto, cuartoAuto, quintoAuto];
+     
+ /*   var select = document.getElementById("exampleFormControlSelect3"); //Seleccionamos el select
       
       for(var i=0; i < BaseDatosAuto[0].anio.length; i++){ 
           var option = document.createElement("option"); //Creamos la opcion
           option.innerHTML = BaseDatosAuto[0].anio[i]; //Metemos el texto en la opción
           select.appendChild(option); //Metemos la opción en el select
-      }
-  
+      }  */
+
      
    var select = document.getElementById("exampleFormControlSelect1"); //Seleccionamos el select
    
@@ -32,12 +80,53 @@
          select.appendChild(option); //Metemos la opción en el select
       }
 
-   var select = document.getElementById("exampleFormControlSelect2"); //Seleccionamos el select
-      
-      for(var i=0; i < BaseDatosAuto[0].modelo.length; i++){ 
-          var option = document.createElement("option"); //Creamos la opcion
-          option.innerHTML = BaseDatosAuto[0].modelo[i]; //Metemos el texto en la opción
-          select.appendChild(option); //Metemos la opción en el select
-      }
 
-    
+      var padre = document.getElementById("padre");
+      var hijo = document.getElementById("hijo");
+      parrafo.parentNode.removeChild();
+ 
+///////////// funcion para tomar modelos y cargar anios
+   
+///////////// evento onchange para cambiar marca y modelo
+   document.getElementById("exampleFormControlSelect1").addEventListener("change", buscarModelos);
+   
+   function buscarModelos(){
+      
+      var buscarMarca = document.getElementById("exampleFormControlSelect1").value;
+     
+      console.log(buscarMarca)
+      switch (buscarMarca){
+         case ("PEUGEOT"):
+            var select = document.getElementById("exampleFormControlSelect2"); //Seleccionamos el select
+
+            
+            for(var i=0; i < BaseDatosAuto[0].modelo.length; i++){ 
+            var option = document.createElement("option"); //Creamos la opcion
+            option.innerHTML = BaseDatosAuto[0].modelo[i].modelo; //Metemos el texto en la opción
+            select.appendChild(option); //Metemos la opción en el select
+   
+         }
+         break;
+
+         case ("CHEVROLET"):
+            var select = document.getElementById("exampleFormControlSelect2"); //Seleccionamos el select
+      
+            for(var i=0; i < BaseDatosAuto[1].modelo.length; i++){ 
+            var option = document.createElement("option"); //Creamos la opcion
+            option.innerHTML = BaseDatosAuto[1].modelo[i].modelo; //Metemos el texto en la opción
+            select.appendChild(option); //Metemos la opción en el select
+         }
+         break;
+      }
+   }
+/////////////evento click para el boton
+   document.getElementById("botonCotizar").addEventListener("click", tomarValores);
+
+   function tomarValores(){
+     
+      var tomarMarca = document.getElementById("exampleFormControlSelect1").value;
+      var tomarModelo = document.getElementById("exampleFormControlSelect2").value;
+      var tomaranio = document.getElementById("exampleFormControlSelect3").value;
+      console.log(tomarMarca+ "  " + tomarModelo + "  " + tomaranio);
+   
+   }  
